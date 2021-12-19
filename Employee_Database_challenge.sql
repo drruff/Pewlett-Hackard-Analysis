a26fd5c7add23d84c52f38a8d2b08d8bc618b6d9
Dylan Ruff <dylan.ruff@mail.utoronto.ca>
2021-12-19 17:07:38 -0500

    adding eligble list of people for mentorship program

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -30,4 +30,21 @@ INTO retiring_titles
 FROM unique_titles AS ut
 GROUP BY ut.title
 ORDER BY ut.count DESC;
-	
+
+-- Creating Mentorship Eligibiliy table
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	tit.title
+INTO mentorship_eligibilty
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS tit
+ON (e.emp_no = tit.emp_no)
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
