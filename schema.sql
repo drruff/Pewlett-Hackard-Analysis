e4aa857b115f7461ac2b9359e9e9d6e244e77fa7
Dylan Ruff <dylan.ruff@mail.utoronto.ca>
2021-12-19 12:59:48 -0500

    fixed schema

--- schema.sql
+++ schema.sql
@@ -36,8 +36,8 @@ CREATE TABLE salaries (
 );
 
 CREATE TABLE dept_emp (
+  emp_no INT NOT NULL
 	dept_no varchar NOT NULL,
-	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
